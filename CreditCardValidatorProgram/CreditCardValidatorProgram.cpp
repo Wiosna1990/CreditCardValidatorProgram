0c7b0bf5c04335380cd676a35d3f0a0305b79a0e
Wiosna1990 <zanzarah@op.pl>
2023-02-23 10:53:47 +0100

    Dodaj pliki projektów.

---
+++ CreditCardValidatorProgram/CreditCardValidatorProgram.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int getDigit(const int number);
+int sumOddDigits(const string cardNumber);
+int sumEvenDigits(const string cardNumber);
+
+int main()
+{
+    string cardNumber;
+    cout << "Enter a credit card number: ";
+    cin >> cardNumber;
+    int result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+    if (result % 10==0)
+    {
+        cout << "The " <<cardNumber <<" is valid!" << endl;
+    }
+    else
+    {
+        cout << "The " << cardNumber << " is not valid!" << endl;
+    }
+    
+}
+int getDigit(const int number)
+{
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigits(const string cardNumber)
+{
+    int sum = 0;
+    for (int i = cardNumber.size() - 1; i >= 0; i -= 2)
+    {
+        sum +=(cardNumber[i]-'0');
+    }
+    return sum;
+}
+int sumEvenDigits(const string cardNumber)
+{
+    int sum = 0;
+    for (int i = cardNumber.size()-2 ; i >=0; i -= 2)
+    {
+        sum += getDigit((cardNumber[i] - '0') * 2);
+    }
+    return sum;
+}
